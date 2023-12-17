5eae5ffbe84d180924a2911ea1d0ac0516c9b066
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-17 20:47:10 -0300

    add shadcn/ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,10 @@
+import { Button } from "~/components/ui/button";
+import { Badge } from "~/components/ui/badge";
+import { Checkbox } from "~/components/ui/checkbox";
+import { Label } from "~/components/ui/label";
+import { CardContent, Card } from "~/components/ui/card";
+import { Link } from "@remix-run/react";
+
 export function headers({
   loaderHeaders,
   parentHeaders,
@@ -9,45 +16,78 @@ export function headers({
     "This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
   );
   return {
-    // This is an example of how to set caching headers for a route
-    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
     "Cache-Control": "public, max-age=60, s-maxage=60",
   };
 }
 
-export default function Index() {
+export default function Component() {
   return (
-    <main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer noopener"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer noopener"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer noopener"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <main className="flex-1 p-6 flex flex-col gap-6 overflow-x-scroll">
+      <div className="flex gap-4">
+        <div className="flex flex-col w-72 gap-4">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-xl font-semibold">To Do</h2>
+            <Button className="ml-auto" size="sm">
+              Add task
+            </Button>
+          </div>
+          <Card>
+            <CardContent className="flex flex-row items-start gap-2 p-4">
+              <Checkbox id="task1" />
+              <div className="space-y-1 leading-none">
+                <Label className="text-lg font-medium" htmlFor="task1">
+                  Finish writing the report
+                </Label>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  Due by 5 PM
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent className="flex flex-row items-start gap-2 p-4">
+              <Checkbox id="task2" />
+              <div className="space-y-1 leading-none">
+                <Label className="text-lg font-medium" htmlFor="task2">
+                  Team Meeting
+                </Label>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  Starts at 2 PM
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="flex flex-col w-72 gap-4">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-xl font-semibold">In Progress</h2>
+            <Button className="ml-auto" size="sm">
+              Add task
+            </Button>
+          </div>
+          <Card>
+            <CardContent className="flex flex-row items-start gap-2 p-4">
+              <Checkbox id="task3" />
+              <div className="space-y-1 leading-none">
+                <Label className="text-lg font-medium" htmlFor="task3">
+                  Submit the proposal
+                </Label>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  Due by end of day
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="flex flex-col w-72 gap-4">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-xl font-semibold">Done</h2>
+            <Button className="ml-auto" size="sm">
+              Add task
+            </Button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- remix.init/remix.config.js
+++ remix.init/remix.config.js
@@ -3,6 +3,8 @@ import { config } from '@netlify/remix-edge-adapter'
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ...config,
+  tailwind: true,
+  postcss: true,
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
   //
