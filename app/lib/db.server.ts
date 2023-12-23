bc0b5932071ff000107c5dffb40ff5550c4db2be
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-23 20:51:11 -0300

    Fix database connection in production environment

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -6,16 +6,16 @@ declare global {
   var db: PrismaClient | undefined;
 }
 
-// if (process.env.NODE_ENV === "production") {
-//   db = new PrismaClient();
-//   db.$connect();
-// } else {
-//   if (!global.db) {
-global.db = new PrismaClient();
-global.db.$connect();
-// }
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+  db.$connect();
+} else {
+  if (!global.db) {
+    global.db = new PrismaClient();
+    global.db.$connect();
+  }
 
-db = global.db;
-// }
+  db = global.db;
+}
 
 export default db;
